19d3256ce6aee0fd7b647f896d169f8db3fd2315
Yasset Perez-Riverol <ypriverol@gmail.com>
2015-11-03 15:46:22 +0000

    some changes in the function to retirve log lists

--- src/main/java/uk/ac/ebi/ddi/ebe/ws/dao/model/common/QueryResult.java
+++ src/main/java/uk/ac/ebi/ddi/ebe/ws/dao/model/common/QueryResult.java
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import uk.ac.ebi.ddi.ebe.ws.dao.model.common.Entry;
 import uk.ac.ebi.ddi.ebe.ws.dao.model.common.Facet;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author Yasset Perez-Riverol ypriverol
  */
@@ -17,11 +22,14 @@ public class QueryResult {
     Integer count = null;
 
     @JsonProperty("entries")
-    Entry[] entries;
+    Entry[] entries = null;
 
     @JsonProperty("facets")
     Facet[] facets;
 
+    public QueryResult(){
+        count = 0;
+    }
     public Integer getCount() {
         return count;
     }
@@ -44,5 +52,44 @@ public class QueryResult {
 
     public void setFacets(Facet[] facets) {
         this.facets = facets;
+    }
+
+    public void addResults(QueryResult results) {
+
+        Set<Entry> entries = new HashSet<>();
+        Set<Facet> facets  = new HashSet<>();
+
+        if(results != null){
+
+            if(this.entries != null)
+                for(Entry entry: this.entries)
+                    entries.add(entry);
+            if(results.entries != null)
+                for(Entry entry: results.entries)
+                    entries.add(entry);
+            if(this.facets != null)
+                for(Facet entry: this.facets)
+                    facets.add(entry);
+            if(results.facets != null)
+                for(Facet entry: results.facets)
+                    facets.add(entry);
+
+            this.facets  = new Facet[facets.size()];
+            this.entries = new Entry[entries.size()];
+
+            int i = 0;
+            for(Entry entry: entries){
+                this.entries[i] = entry;
+                i++;
+            }
+
+            i = 0;
+            for(Facet entry: facets){
+                this.facets[i] = entry;
+                i++;
+            }
+
+            count = entries.size();
+        }
     }
 }
