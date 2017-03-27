a294b04d42be6228b3d03e157e89ab9ad0da9a34
gaurhari <gaurharidass@gmail.com>
2017-03-27 15:58:42 +0100

    added new databases

--- src/main/java/uk/ac/ebi/ddi/ebe/ws/dao/utils/Constans.java
+++ src/main/java/uk/ac/ebi/ddi/ebe/ws/dao/utils/Constans.java
@@ -22,7 +22,11 @@ public class Constans {
         ARRAY_EXPRESS("ArrayExpress", "arrayexpress-repository"),
         METABOLOMEEXPRESS("MetabolomeExpress", "metabolome_express"),
         EXPRESSION_ATLAS("ExpressionAtlas", "atlas-experiments"),
-        METABOLOMICSWORKBENCH("MetabolomicsWorkbench", "metabolomics_workbench");
+        METABOLOMICSWORKBENCH("MetabolomicsWorkbench", "metabolomics_workbench"),
+        BIOMODELS("BioModels Database","BioModels"),
+        LINCS("LINCS","LINCS"),
+        PAXDB("PAXDB","PAXDB"),
+        JPOST("JPOST Repository","JPOST");
 
         String databaseName;
         String solarName;
