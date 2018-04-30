b432751d0eaecdb7b84bd3311cb7301e78b3ba1d
Tung Nguyen <nvntung@gmail.com>
2018-04-30 10:24:12 +0100

    apply reformat code in IntelliJ IDEA

--- src/main/java/uk/ac/ebi/ddi/ebe/ws/dao/client/dataset/DatasetWsClient.java
+++ src/main/java/uk/ac/ebi/ddi/ebe/ws/dao/client/dataset/DatasetWsClient.java
@@ -2,7 +2,6 @@ package uk.ac.ebi.ddi.ebe.ws.dao.client.dataset;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.ac.ebi.ddi.ebe.ws.dao.client.EbeyeClient;
 import uk.ac.ebi.ddi.ebe.ws.dao.config.AbstractEbeyeWsConfig;
 import uk.ac.ebi.ddi.ebe.ws.dao.model.common.QueryResult;
@@ -17,7 +16,7 @@ import java.util.Set;
 /**
  * @author Yasset Perez-Riverol ypriverol
  */
-public class DatasetWsClient extends EbeyeClient{
+public class DatasetWsClient extends EbeyeClient {
 
     private static final Logger logger = LoggerFactory.getLogger(DatasetWsClient.class);
 
@@ -32,30 +31,31 @@ public class DatasetWsClient extends EbeyeClient{
 
     /**
      * Returns the Datasets for a domain with an specific Query
+     *
      * @param domainName Domain to retrieve the information
-     * @param query Web-service query
-     * @param fields fields to be query
-     * @param start  number of the first entry to retrieve
-     * @param size   Number of entries to be retrieve maximum 100.
+     * @param query      Web-service query
+     * @param fields     fields to be query
+     * @param start      number of the first entry to retrieve
+     * @param size       Number of entries to be retrieve maximum 100.
      * @param facetCount Face count the number of facets by entry.
      * @return A list of entries and the facets included
      */
     public QueryResult getDatasets(String domainName, String query, String[] fields,
-                                   String sortField, String order, int start, int size, int facetCount){
+                                   String sortField, String order, int start, int size, int facetCount) {
 
         String finalFields = DDIUtils.getConcatenatedField(fields);
 
-        if((sortField != null && sortField.length()> 0) && (order == null || order.length() == 0))
+        if ((sortField != null && sortField.length() > 0) && (order == null || order.length() == 0))
             order = Constans.ASCENDING;
 
         String url = String.format("%s://%s/ebisearch/ws/rest/%s?query=%s&fields=%s&start=%s&size=%s&facetcount=%s&format=JSON",
                 config.getProtocol(), config.getHostName(), domainName, query,
-                    finalFields, start, size, facetCount, sortField, order);
+                finalFields, start, size, facetCount, sortField, order);
 
-        if(!(sortField == null || sortField.length() == 0 || order == null || order.length()  == 0))
+        if (!(sortField == null || sortField.length() == 0 || order == null || order.length() == 0))
             url = String.format("%s://%s/ebisearch/ws/rest/%s?query=%s&fields=%s&start=%s&size=%s&facetcount=%s&sortfield=%s&order=%s&format=JSON",
                     config.getProtocol(), config.getHostName(), domainName, query,
-                        finalFields, start, size, facetCount, sortField, order);
+                    finalFields, start, size, facetCount, sortField, order);
 
         //Todo: Needs to be removed in the future, this is for debugging
         logger.debug(url);
@@ -65,12 +65,13 @@ public class DatasetWsClient extends EbeyeClient{
 
     /**
      * This query retrieve the specific entries using a set of identifiers from an specific domain
+     *
      * @param domainName domain
-     * @param fields fields to be retrieved from the domain for each specific id
-     * @param ids   the set of ids to be retrieved.
+     * @param fields     fields to be retrieved from the domain for each specific id
+     * @param ids        the set of ids to be retrieved.
      * @return QueryResult
      */
-    public QueryResult getDatasetsById(String domainName, String[] fields, Set<String> ids){
+    public QueryResult getDatasetsById(String domainName, String[] fields, Set<String> ids) {
 
         String finalFields = DDIUtils.getConcatenatedField(fields);
         String[] myIds = ids.toArray(new String[ids.size()]);
@@ -79,7 +80,7 @@ public class DatasetWsClient extends EbeyeClient{
         String database = Constans.Database.retriveSorlName(domainName);
 
         String url = String.format("%s://%s/ebisearch/ws/rest/%s/entry/%s?fields=%s&format=JSON",
-                config.getProtocol(), config.getHostName(), database, finalIds,  finalFields, finalFields);
+                config.getProtocol(), config.getHostName(), database, finalIds, finalFields, finalFields);
 
         return this.restTemplate.getForObject(url, QueryResult.class);
 
@@ -87,13 +88,14 @@ public class DatasetWsClient extends EbeyeClient{
 
     /**
      * This function returns the most frequently terms for an specific field in the database or repository
-     * @param domainName The domain name that will be used
-     * @param field      The specific field for the most frequently terms.
+     *
+     * @param domainName     The domain name that will be used
+     * @param field          The specific field for the most frequently terms.
      * @param exclusionTerms List of terms to be excluded
-     * @param size number of terms to be retrieved
+     * @param size           number of terms to be retrieved
      * @return TermResult
      */
-    public TermResult getFrequentlyTerms(String domainName, String field, String[] exclusionTerms, int size){
+    public TermResult getFrequentlyTerms(String domainName, String field, String[] exclusionTerms, int size) {
 
         String exclusionWord = DDIUtils.getConcatenatedField(exclusionTerms);
 
@@ -103,12 +105,12 @@ public class DatasetWsClient extends EbeyeClient{
         return this.restTemplate.getForObject(url, TermResult.class);
     }
 
-    public SimilarResult getSimilarProjects(String domainName, String id, String[] fields){
+    public SimilarResult getSimilarProjects(String domainName, String id, String[] fields) {
 
         String finalFields = DDIUtils.getConcatenatedField(fields);
 
         String url = String.format("%s://%s/ebisearch/ws/rest/%s/entry/%s/morelikethis/omics?mltfields=%s&excludesets=omics_stopwords&entryattrs=score&format=JSON",
-                config.getProtocol(), config.getHostName(), domainName, id,  finalFields);
+                config.getProtocol(), config.getHostName(), domainName, id, finalFields);
 
         return this.restTemplate.getForObject(url, SimilarResult.class);
 
